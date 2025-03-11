78870a71132f4b2e632e9eedb59f9ab6616b9b81
blockvished <jcmvishael@gmail.com>
2025-03-11 06:01:36 +0000

    setup next reload with wsl

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // Delay before rebuilding
+      ignored: /node_modules/,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
