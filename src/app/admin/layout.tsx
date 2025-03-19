1b918d6c95549edd585ee0eda0bb62a7151b9de4
blockvished <jcmvishael@gmail.com>
2025-03-19 08:08:08 +0000

    updated layout to stay safe

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,16 +5,26 @@ import Sidebar from "./_common/Sidebar";
 import Topbar from "./_common/Topbar";
 import Footer from "./_common/Footer";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   return (
-    <div className="flex bg-gray-50">
-      <Sidebar isCollapsed={isCollapsed} />
-      <div className={`flex-1 flex flex-col p-6 transition-all ${isCollapsed ? "ml-20" : "ml-60"}`}>
-        <Topbar onToggleSidebar={() => setIsCollapsed(!isCollapsed)} />
-        <main className="flex-1 p-6">{children}</main>
-        <Footer />
+    <div className=" bg-gray-50">
+      <div className="flex h-screen">
+        <Sidebar isCollapsed={isCollapsed} />
+        <div
+          className={`flex-1 flex flex-col p-6 transition-all ${
+            isCollapsed ? "ml-20" : "ml-60"
+          }`}
+        >
+          <Topbar onToggleSidebar={() => setIsCollapsed(!isCollapsed)} />
+          <main className="flex-1 p-6">{children}</main>
+          <Footer />
+        </div>
       </div>
     </div>
   );
