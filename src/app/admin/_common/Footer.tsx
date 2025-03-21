c85128f78aa07c4942aa0c28587b064548497fd9
blockvished <jcmvishael@gmail.com>
2025-03-21 10:38:02 +0000

    added text in footer

--- src/app/admin/_common/Footer.tsx
+++ src/app/admin/_common/Footer.tsx
@@ -4,9 +4,8 @@ const Footer: React.FC = () => {
   return (
     <footer className="py-3 px-4 text-center text-sm text-gray-500 border-t border-gray-200">
       <div className="flex flex-col md:flex-row justify-between items-center">
-        <p>© 2025 Digambar Healthcare. All rights reserved.</p>
-        <div className="mt-2 md:mt-0 flex space-x-4">
-        </div>
+        <p>© 2025 Digambar Healthcare. All rights reserved. An
+        Initiative of Prgenix</p>
       </div>
     </footer>
   );
