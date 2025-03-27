56c6057331c481290aeda307aac82a8e3e0f7758
blockvished <jcmvishael@gmail.com>
2025-03-27 07:00:47 +0000

    create payout create form

--- src/app/admin/payouts/add/page.tsx
+++ src/app/admin/payouts/add/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
