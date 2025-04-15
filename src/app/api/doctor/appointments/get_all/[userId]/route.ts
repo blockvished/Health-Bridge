b9251b51e4baecd93561eb883579b92f95cc0464
blockvished <jcmvishael@gmail.com>
2025-04-15 06:19:47 +0000

    get all appointments api

---
+++ src/app/api/doctor/appointments/get_all/[userId]/route.ts
@@ -0,0 +1,83 @@
+import { NextRequest, NextResponse } from "next/server";
+import { or, eq } from "drizzle-orm";
+import {
+  doctor,
+  appointments,
+  NewAppointment,
+  users,
+  patient,
+  genderEnum,
+} from "../../../../../../db/schema";
+import db from "../../../../../../db/db";
+import { verifyAuthToken } from "../../../../../lib/verify";
+import Appointments from "@/admin/appointment/page";
+
+// =======================
+// Get - All Appointments for a Doctor
+// =======================
+export async function GET(req: NextRequest) {
+  // Get ID from URL
+  const userIdFromUrl = req.nextUrl.pathname.split("/").pop() || "unknown";
+
+  // Verify JWT token using the modularized function
+  const decodedOrResponse = await verifyAuthToken();
+
+  // Handle potential error response from token verification
+  if (decodedOrResponse instanceof NextResponse) {
+    return decodedOrResponse;
+  }
+
+  const decoded = decodedOrResponse;
+  const userId = Number(decoded.userId);
+
+  // Check if the requested ID matches the authenticated user's ID
+  if (String(userId) !== userIdFromUrl) {
+    return NextResponse.json(
+      { error: "Forbidden: You don't have access to this profile" },
+      { status: 403 }
+    );
+  }
+
+  // Query for doctor information
+  const doctorData = await db
+    .select()
+    .from(doctor)
+    .where(eq(doctor.userId, userId));
+
+  if (!doctorData.length) {
+    return NextResponse.json(
+      { error: "Doctor profile not found for this user." },
+      { status: 404 }
+    );
+  }
+
+  const requiredDoctorId = doctorData[0].id;
+
+  try {
+    const appointmentsData = await db
+      .select({
+        appointmentId: appointments.id,
+        date: appointments.date,
+        timeFrom: appointments.timeFrom,
+        timeTo: appointments.timeTo,
+        mode: appointments.mode,
+        reason: appointments.reason,
+        
+        visitStatus: appointments.visitStatus,
+        paymentStatus: appointments.paymentStatus,
+
+        patientId: patient.id,
+        patientName: users.name,
+        patientEmail: users.email,
+        patientPhone: users.phone
+      })
+      .from(appointments)
+      .innerJoin(patient, eq(appointments.patientId, patient.id))
+      .innerJoin(users, eq(patient.userId, users.id))
+      .where(eq(appointments.doctorId, requiredDoctorId));
+    return NextResponse.json({ appointments: appointmentsData });  
+  } catch (error) {
+    console.error("Error fetching appointment of doctor:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
