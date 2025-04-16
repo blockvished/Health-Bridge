ad4e986b82a12c5a7e028667812d3002e4dae2bb
blockvished <jcmvishael@gmail.com>
2025-04-16 08:24:57 +0000

    apply prettier

--- src/app/admin/appointment/all_list/page.tsx
+++ src/app/admin/appointment/all_list/page.tsx
@@ -125,13 +125,13 @@ export default function AppointmentsPage() {
     <>
       {editingAppointment ? (
         <div className="bg-white p-6 rounded-lg shadow-md m-auto w-fit">
-  <AppointmentEditForm
-    appointment={editingAppointment}
-    userId={userId}
-    onClose={handleCloseEditForm}
-    onSuccess={handleAppointmentUpdated}
-  />
-</div>
+          <AppointmentEditForm
+            appointment={editingAppointment}
+            userId={userId}
+            onClose={handleCloseEditForm}
+            onSuccess={handleAppointmentUpdated}
+          />
+        </div>
       ) : (
         <div className="bg-white p-6 rounded-lg shadow-md bg-white p-6 m-8 rounded-lg shadow">
           {/* Header */}
